7725d8b260f2e57e7a1b1294d53bef947cb4e752
nd <git@notandy.de>
2018-03-24 23:32:00 +0100

    add raw graph to average

--- average_calculation.py
+++ average_calculation.py
@@ -5,16 +5,25 @@ import datetime
 
 
 def plot(style):
-    days = {}
-    for d, vs in data.items():
-        days[d] = calc.day_avg(vs, style)
-    x, y = zip(*sorted(days.items(), key=lambda x: x[0]))
-    x = [datetime.datetime.fromordinal(i) for i in x]
-    plt.plot(x, y, label=style)
+	if style == "raw":
+		x = []
+		y = []
+		for i in data:
+			for j in data[i]:
+				x.append(datetime.datetime.fromtimestamp(j[0]/1000))
+				y.append(j[1])
+	else:
+		days = {}
+		for d, vs in data.items():
+			days[d] = calc.day_avg(vs, style)
+		x, y = zip(*sorted(days.items(), key=lambda x: x[0]))
+		x = [datetime.datetime.fromordinal(i) for i in x]
+	plt.plot(x, y, label=style)
 
 
 if __name__ == "__main__":
     data = calc.read_data()
+    plot("raw")
     plot("trapezium")
     plot("old")
     legend = plt.legend()
