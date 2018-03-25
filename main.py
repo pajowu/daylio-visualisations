eb9e7f3873e858c7ade80c2c5d494ec5e6ce78e9
pajowu <git@ca.pajowu.de>
2018-03-25 19:41:39 +0200

    Add option to show multiple vizualisations in plot

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import importlib
 import textwrap
 from matplotlib import pyplot
 import calc
+import math
 
 VIZS = ["year_in_pixels", "average_calculation", "entries_per_day", "mood_graph"]
 
@@ -16,9 +17,10 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='daylio visualizations',
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-t', '--type', dest='viz_type',
-	                    default="year_in_pixels",
-	                    help=('What visualizations type shall be run. '
-	                          'For a list of available ones use `--type LIST`'))
+	                    default="ALL",
+	                    help=('What visualizations type shall be run as a comma seperated list. '
+	                          'For a list of available ones use `--type LIST`. By default all plots '
+	                          'are shown'))
 
 	parser.add_argument('--random-data', action="store_true", help=("If this option is specified, "
 		"the mood data will be generated randomly instead of reading the entries.db"))
@@ -30,7 +32,10 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 
-	if args.viz_type == "LIST":
+	viz_to_run = args.viz_type.split(",")
+	if viz_to_run == ["ALL"]: viz_to_run = VIZS
+
+	if args.viz_type == ["LIST"]:
 		for viz in VIZS:
 			module = load_viz(viz)
 			docstring = "No documentation given"
@@ -38,17 +43,24 @@ if __name__ == '__main__':
 				docstring = module.__doc__
 			print(textwrap.fill("{}: {}".format(viz, docstring),
 				initial_indent="", subsequent_indent="    "))
-	elif args.viz_type in VIZS:
-		module = load_viz(args.viz_type)
+	elif set(viz_to_run) - set(VIZS) == set():
+		px = math.sqrt(len(viz_to_run))
+		py = math.ceil(len(viz_to_run) / px)
+		pyplot.figure(figsize=(6.4*px, 4.8*py))
 		if args.random_data:
 			data = calc.read_random()
 		else:
 			data = calc.read_sqlite(args.sqlite_file)
-		module.plot(pyplot, data)
+		for pi,viz in enumerate(viz_to_run):
+			pyplot.subplot(px,py,pi+1)
+			module = load_viz(viz)
+			module.plot(pyplot, data)
+			pyplot.title(" ".join(viz.split("_")).title())
 		if args.output:
 			pyplot.tight_layout()
+
 			pyplot.savefig(args.output, dpi=300)
 		else:
 			pyplot.show()
 	else:
-		print("Invalid visualization specified")
+		print("Invalid visualization(s) specified: {}".format(set(viz_to_run) - set(VIZS)))
